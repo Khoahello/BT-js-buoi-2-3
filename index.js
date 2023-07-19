8bc1c9e584a40b372be3025d444bf225afaa40fa
Khoahello <dinhtankhoa2k7@gmail.com>
2023-07-19 23:30:18 +0700

    update giai bai tap

--- index.js
+++ index.js
@@ -3,7 +3,11 @@
 /**
  * Đầu vào: số ngày làm
  * 
- * Các bước xử lý
+ * Các bước xử lý:
+ * - Khai báo biến và gán giá trị cho biến ngay
+ * - Khai báo biến luong
+ * - Tính lương theo công thức: luong = 100000 * ngay
+ * In giá trị của biến luong ra trang web
  * 
  * Đầu ra: lương
  */
@@ -19,17 +23,21 @@ function tinh_luong() {
 /**
  * Đầu vào: 5 số thực
  * 
- * Các bước xử lý
+ * Các bước xử lý:
+ * - Khai báo biến và gán giá trị số cho biến so1, so2,...
+ * - Khai báo biến trungbinh
+ * - Tính trung bình theo công thức: trungbinh = (so1 + so2 + so3 + so4 +so5) / 5
+ * - In giá trị của biến trungbinh ra trang web
  * 
  * Đầu ra: trung bình của 5 số thực
  */
 
 function tinh_trungbinh() {
-    var so1 = Number(document.getElementById("so1").value)
-    var so2 = Number(document.getElementById("so2").value)
-    var so3 = Number(document.getElementById("so3").value)
-    var so4 = Number(document.getElementById("so4").value)
-    var so5 = Number(document.getElementById("so5").value)
+    var so1 = Number(document.getElementById("so_1").value)
+    var so2 = Number(document.getElementById("so_2").value)
+    var so3 = Number(document.getElementById("so_3").value)
+    var so4 = Number(document.getElementById("so_4").value)
+    var so5 = Number(document.getElementById("so_5").value)
     var trungbinh = document.getElementById("trung binh")
     trungbinh.innerText = (so1 + so2 + so3 + so4 + so5) / 5
 }
@@ -39,7 +47,11 @@ function tinh_trungbinh() {
 /**
  * Đầu vào: số tiền USD
  * 
- * Các bước xử lý
+ * Các bước xử lý:
+ * - Khai báo và gán giá trị cho biến tienUSD
+ * - Khai báo biến tienVND
+ * - Tính số tiền VND theo công thức: tienVND = tienUSD * 23500
+ * - In giá trị của biến tienVND ra trang web
  * 
  * Đầu ra: số tiền VND
  */
@@ -55,7 +67,14 @@ function doitien() {
 /**
  * Đầu vào: chiều dài, chiều rộng
  * 
- * Các bước xử lý
+ * Các bước xử lý:
+ * - Khai báo và gán giá trị số cho biến chieudai
+ * - Khai báo và gán giá trị số cho biến chieurong
+ * - Khai báo biến dientich
+ * - Khai báo biến chuvi
+ * - Tính diện tích theo công thức: dientich = chieudai * chieurong
+ * - Tính chu vi theo công thức: chuvi = chieudai * chieurrong
+ * - In giá trị của biến dientich và biến chuvi ra trang web
  * 
  * Đầu ra: chu vi, diện tích
  */
@@ -64,9 +83,9 @@ function tinh_chuvidientich() {
     var chieudai = Number(document.getElementById("chieudai").value)
     var chieurong = Number(document.getElementById("chieurong").value)
     var dientich = document.getElementById("dientich")
-    dientich.innerText = chieudai * chieurong
     var chuvi = document.getElementById("chuvi")
-    chuvi.innerText = (chieudai + chieurong)
+    dientich.innerText = chieudai * chieurong
+    chuvi.innerText = chieudai + chieurong
 }
 
 // Bài 5
@@ -74,7 +93,13 @@ function tinh_chuvidientich() {
 /**
  * Đầu vào: số có 2 chữ số
  * 
- * Các bước xử lý
+ * Các bước xử lý:
+ * - Khai báo và gán giá trị cho biến so
+ * - Lấy số hàng đơn vị bằng công thức: so_hang_dv = so % 10
+ * - Lấy số hàng chục bằng công thức: so_hang_chuc = Math.floor (so / 10)
+ * - Khai báo biến tong_2_ky_so
+ * - Tính tổng 2 ký số bằng công thức: tong_2_ky_so = so_hang_dv + so_hang_chuc
+ * - In ra giá trị của biến tong_2_ky_so
  * 
  * Đầu ra: tổng 2 ký số
  */
